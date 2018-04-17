a1f5acaffb08b4e48ee21eefb9dfa5ddbe48df8e
boondoggler <cedric_bertolasio@trimble.com>
2018-04-17 16:51:49 -0600

    adding initial commit

---
+++ udemy/ucmsapi/ucmsapi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ucmsapi.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string IsAdmin { get; set; }
+	}
+}
