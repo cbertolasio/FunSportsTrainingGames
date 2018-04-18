58172eb8cb4a11a2aff5ac55b552b65ea2962deb
boondoggler <cedric_bertolasio@trimble.com>
2018-04-18 14:26:13 -0600

    adding code through step 10 of the tutorial - navbar links

---
+++ udemy/ucmsapi/ucmsapi/Controllers/PagesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ucmsapi.Data;
+using ucmsapi.Models;
+
+
+namespace ucmsapi.Controllers
+{
+	[Route("api/[controller]")]
+	public class PagesController : Controller
+	{
+		private readonly UCmsApiContext _context;
+
+
+		// GET api/pages
+		public IActionResult Get()
+		{
+			List<Page> pages = _context.Pages.ToList();
+
+			return Json(pages);
+		}
+
+		// GET api/pages/slug
+		[HttpGet("{slug}")]
+		public IActionResult Get(string slug)
+		{
+			Page page = _context.Pages.SingleOrDefault(it => it.Slug == slug);
+			if (page == null)
+			{
+				return Json("PageNotFound");
+			}
+
+			return Json(page);
+		}
+
+
+
+		// POST api/<controller>
+		[HttpPost]
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/<controller>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+
+		public PagesController(UCmsApiContext context)
+		{
+			_context = context;
+		}
+	}
+}
--- udemy/ucmsapi/ucmsapi/Startup.cs
+++ udemy/ucmsapi/ucmsapi/Startup.cs
@@ -1,43 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using ucmsapi.Data;
 
 namespace ucmsapi
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(options => options.AddPolicy("AllowAll", 
+				p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
 			services.AddDbContext<UCmsApiContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc();
-        }
+			services.AddMvc();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseCors("AllowAll");
 
-            app.UseMvc();
-        }
-    }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
 }
